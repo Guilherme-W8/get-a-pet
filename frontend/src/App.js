b75e9ee7f1b4d381660021734d2d99c6d3d5b25f
guilherme-w8 <gtavares739@gmail.com>
2024-11-11 17:25:18 -0300

    React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+
+/* pages */
+import Home from './components/pages/Home';
+import Login from './components/pages/Auth/Login';
+import Register from './components/pages/Auth/Register';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Get A Pet</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
