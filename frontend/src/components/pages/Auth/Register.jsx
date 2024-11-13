12699af27a5d08e3df32bf901db492bc9b990a3e
guilherme-w8 <gtavares739@gmail.com>
2024-11-13 10:01:54 -0300

    criando objeto de user

--- frontend/src/components/pages/Auth/Register.jsx
+++ frontend/src/components/pages/Auth/Register.jsx
@@ -1,18 +1,24 @@
 import Input from "../../form/Input";
 import styles from "../../form/Form.module.css";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Register() {
+    const [user, setUser] = useState();
 
     function handleChange(e) {
-        // setUser({ ...user, [e.target.name]: e.target.value })
+        setUser({ ...user, [e.target.name]: e.target.value });
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
     }
 
     return (
         <section className={styles.form_container}>
             <h1>Cadastro</h1>
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <Input
                     text="Nome"
                     type="text"
