17ab4bb0dc0495f1dee868bccbf2ee16f970874f
guilherme-w8 <gtavares739@gmail.com>
2024-11-13 10:15:17 -0300

    add auth hook

---
+++ frontend/src/hooks/useAuth.js
@@ -0,0 +1,21 @@
+// API
+import api from '../utils/api.js';
+
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function useAuth() {
+    async function register(user) {
+        try {
+            const data = await api.post('/users/register', user).then((response) => {
+                return response.data;
+            });
+
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return { register };
+}
