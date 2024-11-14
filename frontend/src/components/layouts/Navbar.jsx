a61416be40c16cd5f30fa9f05053263735ca91f4
guilherme-w8 <gtavares739@gmail.com>
2024-11-14 14:51:52 -0300

    persistind autenticacao no front-end

--- frontend/src/components/layouts/Navbar.jsx
+++ frontend/src/components/layouts/Navbar.jsx
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom";
+import React, { useContext } from 'react';
 import styles from './Navbar.module.css';
 
 import Logo from '../../assets/img/logo.png';
 
+/* context */
+import { Context } from '../../context/UserContext';
+
 function Navbar() {
+    const { authenticated, logout } = useContext(Context);
+
     return (
         <nav className={styles.navbar}>
             <div className={styles.navbar_logo}>
@@ -16,13 +22,29 @@ function Navbar() {
                     <Link to="/">Adotar</Link>
                 </li>
 
-                <li>
-                    <Link to="/login">Entrar</Link>
-                </li>
-
-                <li>
-                    <Link to="/register">Cadastrar</Link>
-                </li>
+                {authenticated ? (
+                    <>
+                        <li>
+                            <Link to="/pet/myadoptions">Minhas Adoções</Link>
+                        </li>
+                        <li>
+                            <Link to="/pet/mypets">Meus Pets</Link>
+                        </li>
+                        <li>
+                            <Link to="/user/profile">Meu Perfil</Link>
+                        </li>
+                        <li onClick={logout}>Sair</li>
+                    </>
+                ) : (
+                    <>
+                        <li>
+                            <Link to="/login">Entrar</Link>
+                        </li>
+                        <li>
+                            <Link to="/register">Registar</Link>
+                        </li>
+                    </>
+                )}
             </ul>
         </nav>
     )
